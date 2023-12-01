00b6591c4cba01d4054213e945d7662790a1829c
Juliette Luhrs <luhrsj@roverkids.org>
2023-12-01 14:06:24 -0500

    2.1 finish

--- Prototype 3/Assets/Scripts/PlayerControl.cs
+++ Prototype 3/Assets/Scripts/PlayerControl.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
     // Update is called once per frame
     void Update()
     {
+        // Keep the player in bounds
+        if (transform.position.x < -xRange)
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    
         if (transform.position.x < -xRange)
-        {
         
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z); )
-        }  
-}          
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+    }
+}
